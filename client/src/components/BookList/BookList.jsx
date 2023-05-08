f79d82f4fdf3ae2c56a978a353d391d4b36a0594
NTumminaro <nicholastumminaro@gmail.com>
2023-05-08 18:42:37 -0500

    adds swiper component to book lists, needs mode styling

--- client/src/components/BookList/BookList.jsx
+++ client/src/components/BookList/BookList.jsx
@@ -1,6 +1,11 @@
 import { Box } from "@mui/material";
 import Book from "../Book/Book";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./booklist.scss"
+import { Pagination } from "swiper";
 
 let userBooks = [
     {
@@ -20,7 +25,7 @@ let userBooks = [
     },
     {
         title: "Divine Comedy234234",
-        author: "Dante Alighieri",
+        author: "Dante poop",
         image: "/images/content.jpg",
     },
     {
@@ -37,14 +42,29 @@ let userBooks = [
 
 function BookList() {
     return (
-        <Box display={"flex"} justifyContent={"center"}>
-            {/* some sort of component for the infinite scroll */}
-            {/* this will pass each book of the user as a prop and create a book component for each */}
+        // <Box display={"flex"} justifyContent={"center"}>
+        //     {/* some sort of component for the infinite scroll */}
+        //     {/* this will pass each book of the user as a prop and create a book component for each */}
+        //     {userBooks.map((book) => (
+        //             <Book key={book.title} book={book}/>
+        //     ))}
+        //     {/* some sort of component for the infinite scroll */}
+        //     </Box>
+        <Swiper
+            slidesPerView={4}
+            centeredSlides={true}
+            spaceBetween={30}
+            grabCursor={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+        >
             {userBooks.map((book) => (
-                    <Book key={book.title} book={book}/>
+                <SwiperSlide>{book.title}</SwiperSlide>
             ))}
-            {/* some sort of component for the infinite scroll */}
-            </Box>
+        </Swiper>
     );
 }
 
