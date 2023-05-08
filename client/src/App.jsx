c9828baf977b31111ee29006228c876f5b75a5e4
Sealcakes <taylorcseal@gmail.com>
2023-05-08 16:22:59 -0500

    changes layout of client dashboard up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,29 @@ import Wall from "./components/Wall/Wall";
 import Bookshelf from "./components/Bookshelf/Bookshelf";
 import Navbar from "./components/Navbar/Navbar";
 import { Box } from "@mui/material";
+import { Routes, Route } from 'react-router-dom';
+import Layout from './pages/layout';
+import Dashboard from './pages/dashboard/dashboard';
 
 function App() {
   return (
 
-      <Box  sx={{
-        backgroundImage: `url("/images/wallpaper.jpeg")`,
-        backgroundSize: "fill"
-      }}>
-        <Navbar />
-        <Wall />
-        <Bookshelf />
-      </Box>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
+
+      // <Box  sx={{
+      //   backgroundImage: `url("/images/wallpaper.jpeg")`,
+      //   backgroundSize: "fill"
+      // }}>
+      //   <Navbar />
+      //   <Wall />
+      //   <Bookshelf />
+      // </Box>
 
   );
 }
