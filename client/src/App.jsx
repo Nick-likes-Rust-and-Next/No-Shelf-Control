bfaabc7788895128b845465fa29d3f12cd0d213f
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-05-08 18:26:20 -0500

    added useQuery for booklist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/layout";
 import Dashboard from "./pages/dashboard/dashboard";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [mode, setMode] = useState(true);
@@ -20,11 +23,13 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Routes>
-          <Route path="/" element={<Layout themeSwitch={changeTheme} />}>
-            <Route index element={<Dashboard />} />
-          </Route>
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<Layout themeSwitch={changeTheme} />}>
+              <Route index element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
