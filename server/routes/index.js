4158fb414c6de4a9c1a218267985a8ce865caeb8
NotEnoughBacon <jboyk3@hotmail.com>
2023-05-08 18:09:01 -0600

    uh oh i don messed up A A RON

--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const apiRoute = require('./api');
-
-router.use('/api', apiRoute);
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
